d9dd8c536c5fa0352ff0ce0727da0ae28e917c0d
Surya-Raghuram <suryaraghuramg@gmail.com>
2026-08-06 02:31:09 +0530

    Update site URL in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://your-deployment-url.com',
+  site: 'https://portfolio.suryaraghuramg.workers.dev',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
